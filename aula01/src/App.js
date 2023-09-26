c52df3c60361277b4d2fd22dbdaef9e8c265a16d
Lucas Knupp <lucas_knupp@outlook.com>
2023-09-25 22:14:28 -0300

    http course

--- aula01/src/App.js
+++ aula01/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import ExpenseList from './components/Expenses/ExpenseList';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -25,7 +26,7 @@ function App() {
   ];
   return (
     <div className="App">
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <div className="expenses">
         <ExpenseList expenses={expenses}/>
       </div>
