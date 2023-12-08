3ba86fca487545fdb1cdcf515daddc191e90932a
Lucas Knupp Leite Pessoa <lknupp@btcreditos.com.br>
2023-12-08 12:13:03 -0300

    adicionada componetes de perguntas e respostas
    bug de reformulação de componentes

---
+++ Quiz-App/src/components/Answers.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from "react";
+
+function Answers({answers, selectedAnswers, answerState, onSelect}) {
+    const shuffledAnswers = useRef();
+    if (!shuffledAnswers.current) {
+        shuffledAnswers.current = [...answers];
+        shuffledAnswers.current.sort(() => Math.random() - 0.5);
+      }
+
+  return (
+    <ul id="answers">
+      {shuffledAnswers.current.map((answer) => {
+        const isSelected = selectedAnswers === answer;
+        let cssClasses = "";
+
+        if (answer === "answered" && isSelected) {
+          cssClasses = "selected";
+        }
+
+        if (
+          (answerState === "correct" || answerState === "wrong") &&
+          isSelected
+        ) {
+          cssClasses = answerState;
+        }
+
+        return (
+          <li key={answer} className="answer">
+            <button
+              onClick={() => onSelect(answer)}
+              className={cssClasses}
+            >
+              {answer}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+export default Answers;
--- Quiz-App/src/components/Quiz.jsx
+++ Quiz-App/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useState } from "react";
 
 import QUESTIONS from "../questions";
@@ -6,22 +6,39 @@ import QUESTIONS from "../questions";
 import quizIsCompleteImg from "../assets/quiz-complete.png";
 import QuestionTimer from "./QuestionTimer";
 import { useCallback } from "react";
+import Answers from "./Answers";
+import Question from "./Question";
 
 function Quiz() {
+  const [answerState, setAnswerState] = useState("");
   const [userAnswers, setUserAnswers] = useState([]);
 
-  const activeQuestionIndex = userAnswers.length;
+  const activeQuestionIndex =
+    answerState === "" ? userAnswers.length : userAnswers.length - 1;
 
   const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
-  const handleSelectAnswer = useCallback(function handleSelectAnswer(
-    userAnswer
-  ) {
-    setUserAnswers((prevAnswer) => {
-      return [...prevAnswer, userAnswer];
-    });
-  },
-  []);
+  const handleSelectAnswer = useCallback(
+    function handleSelectAnswer(userAnswer) {
+      setAnswerState("answered");
+      setUserAnswers((prevAnswer) => {
+        return [...prevAnswer, userAnswer];
+      });
+
+      setTimeout(() => {
+        if (userAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+          setAnswerState("correct");
+        } else {
+          setAnswerState("wrong");
+        }
+
+        setTimeout(() => {
+          setAnswerState("");
+        }, 2000);
+      }, 1000);
+    },
+    [activeQuestionIndex]
+  );
 
   const handleSkipAnswer = useCallback(() => {
     handleSelectAnswer(null);
@@ -36,24 +53,9 @@ function Quiz() {
     );
   }
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-  shuffledAnswers.sort(() => Math.random() - 0.5);
-
   return (
     <div id="quiz">
-      <div id="question">
-        <QuestionTimer key={activeQuestionIndex} timeout={10000} onTimeout={handleSkipAnswer} />
-        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-        <ul id="answers">
-          {shuffledAnswers.map((answer) => (
-            <li key={answer} className="answer">
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <Question key={activeQuestionIndex} questionText={QUESTIONS[activeQuestionIndex].text} answers={QUESTIONS[activeQuestionIndex].answers} onSelectAnswer={handleSelectAnswer} answerState={answerState} selectedAnswer={userAnswers[userAnswers.length - 1]} onSkipAnswer={handleSkipAnswer}/>
     </div>
   );
 }
